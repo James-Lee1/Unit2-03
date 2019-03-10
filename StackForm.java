eb17aa8e139d69ef43d98b5a6c476cf4cefd78c3
James Sanii <james@ip-172-31-8-86.ca-central-1.compute.internal>
2019-03-10 02:52:34 +0000

    Updated comments

--- StackForm.java
+++ StackForm.java
@@ -1,3 +1,13 @@
+
+/****************************************************************************
+ *
+ * Created by: James Lee
+ * Created on: Mar 2019
+ * Created for: ICS4U
+ * This program uses the class Stack, and can either push or pop values
+ *
+ ****************************************************************************/
+
 import java.util.Scanner;
 
 class StackForm
@@ -13,10 +23,13 @@ class StackForm
             System.out.print("What would you like to do? (push or pop) : ");
             userChoice = userInput.nextLine();
 
+            // Pops the last value of stack
             if (userChoice.equals("pop"))
             {
                 System.out.println(stack.pop());
             }
+            // Pushes a value inputted by user, and will return an error message if it is
+            // not a number
             else if (userChoice.equals("push"))
             {
                 String pushValue;
@@ -24,6 +37,7 @@ class StackForm
                 pushValue = userInput.nextLine();
                 stack.push(pushValue);
             }
+            // Prints out the entire stack
             System.out.println("The values in the stack are : " + "\n" + stack.toString());
         }
     }
