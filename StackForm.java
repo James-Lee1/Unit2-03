00e36daa3dbfbf1b2bbb77476fcfe7217679c034
James Sanii <james@ip-172-31-8-86.ca-central-1.compute.internal>
2019-03-22 22:07:37 +0000

    Final version

--- StackForm.java
+++ StackForm.java
@@ -1,4 +1,3 @@
-
 /****************************************************************************
  *
  * Created by: James Lee
@@ -14,19 +13,28 @@ class StackForm
 {
     public static void main(String[] args)
     {
+        // Creates new stack
         Stack stack = new Stack();
         Scanner userInput = new Scanner(System.in);
         String userChoice;
 
         while (true)
         {
-            System.out.print("What would you like to do? (push or pop) : ");
+            System.out.print("What would you like to do? (push, pop, or exit) : ");
             userChoice = userInput.nextLine();
+            System.out.print("\n");
 
             // Pops the last value of stack
             if (userChoice.equals("pop"))
             {
-                System.out.println(stack.pop());
+                if (stack.size() == 0)
+                {
+                    System.out.println("Stack is empty!" + "\n");
+                }
+                else
+                {
+                    System.out.println(stack.pop() + " was removed." + "\n");
+                }
             }
             // Pushes a value inputted by user, and will return an error message if it is
             // not a number
@@ -35,10 +43,48 @@ class StackForm
                 String pushValue;
                 System.out.print("Enter an input to push : ");
                 pushValue = userInput.nextLine();
-                stack.push(pushValue);
+
+                if (isValidInput(pushValue) == true)
+                {
+                    stack.push(Integer.parseInt(pushValue));
+                }
+                else
+                {
+                    System.out.print("Error : Not a valid input!" + "\n");
+                }
+            }
+
+            else if (userChoice.equals("exit"))
+            {
+                break;
+            }
+            else
+            {
+                System.out.print("Error : Not a valid input!" + "\n");
             }
             // Prints out entire stack
             System.out.println("The values in the stack are : " + "\n" + stack.toString());
+        }
+    }
+
+    public static boolean isValidInput(String str)
+    {   // Checks if string is a valid number
+        try
+        {
+            int i = Integer.parseInt(str);
+            if (i >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        catch(NumberFormatException nfe)
+        {
+            return false;
         }
     }
 }
